bd365c91dd3cfb83d9de114535738d9f85f928af
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-02-12 20:31:11 -0800

    fixing index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
-const apiRoutes = require('./api/');
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
-router.use('/api', apiRoutes);
 router.use('/posts', postRoutes);
 module.exports = router;
