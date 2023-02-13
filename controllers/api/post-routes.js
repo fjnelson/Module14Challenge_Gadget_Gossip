54b3362cb6fade1cfa39a1b4d27674ee84e7066a
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-02-12 20:23:36 -0800

    adding post route

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll();
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const postData = await Post.create({
+            ...req.body,
+        });
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+})
+
+module.exports = router;
