8abadd8fb3065f175d6f4e964109d4775aeba617
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-02-12 17:40:34 -0800

    adding models

---
+++ models/index.js
@@ -0,0 +1,15 @@
+
+const User = require('./User');
+const Post = require('./Post');
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+module.exports = { User, Post };
